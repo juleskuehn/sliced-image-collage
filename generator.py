a2daee6d3717a7338be199cac6187bb58738ee45
Jules Kuehn <jk@jules.lol>
2019-02-13 13:16:53 -0500

    Dither partly working. Need to propagate outside of slice

--- generator.py
+++ generator.py
@@ -17,6 +17,7 @@ class Generator:
     def __init__(self, targetImg, charSet, shapeliness=0.5,
                         targetShape=None, targetPadding=None, dither=False):
         self.targetImg = targetImg
+        self.ditherImg = targetImg.copy()
         self.charSet = charSet
         self.comboSet = ComboSet()
         self.comboH, self.comboW = charSet.get(0).cropped.shape[0]//2, charSet.get(0).cropped.shape[1]//2
@@ -35,6 +36,7 @@ class Generator:
             'positionsVisited': 0,
             'comparisonsMade': 0
         }
+        self.dither = True
 
     def getSliceBounds(self, row, col):
         startY = row * self.comboH
@@ -50,50 +52,185 @@ class Generator:
     def putBestAdj(self, row, col):
         self.stats['positionsVisited'] += 1
         startX, startY, endX, endY = self.getSliceBounds(row, col)
-        targetSlice = self.targetImg[startY:endY, startX:endX]
-        # Brighten the target to match maximum typable in first overtype
-        targetSlice = gammaCorrect(targetSlice, self.maxGamma)
         # Get ID of best match
-        bestMatch = self.getBestAdj(targetSlice, row, col)
+        # If dither, this will call applyDither on every mockup before scoring
+        bestMatch = self.getBestAdj(row, col)
         # print(bestMatch)
         # Has it changed?
-        
         if self.comboGrid.get(row, col)[3] != bestMatch:
+            changed = True
             # print(self.comboGrid.get(row, col), bestMatch)
             self.comboGrid.put(row, col, bestMatch)
             self.mockupImg[startY:endY, startX:endX] = self.compositeAdj(row, col)
-            return True
         else:
-            self.comboGrid.clean(row, col)
-            return False
+            changed = False
+            # self.comboGrid.clean(row, col)
+
+        self.ditherImg = self.applyDither(row, col)
+        return changed
+
+        # Try compositing different characters onto a copy of the mockupSlice
+    # Compare each to the targetSlice
+    # Return the id of the best matching character
+    def getBestAdj(self, row, col):
+
+        # Score against temporary ditherImg created for this comparison
+        def compare(row, col, ditherImg):
+            startX, startY, endX, endY = self.getSliceBounds(row, col)
+            targetSlice = ditherImg[startY:endY, startX:endX]
+            mockupSlice = self.compositeAdj(row, col)
+            # brighten targetSlice
+            err = abs(np.average(targetSlice/self.maxGamma - mockupSlice))
+            targetSlice = gammaCorrect(targetSlice, self.maxGamma)
+            mse = np.sqrt(compare_mse(targetSlice, mockupSlice))
+            # print('err:',err,'mse:',mse)
+            return err + mse
+
+        chars = self.charSet.getAll()
+        scores = {}
+        origGrid = self.comboGrid.grid.copy()
+        for char in chars:
+            self.stats['comparisonsMade'] += 1
+            self.comboGrid.put(row, col, char.id)
+            ditherImg = self.applyDither(row, col)
+            # Score the composite
+            scores[char.id] = compare(row, col, ditherImg)
+
+        self.comboGrid.grid = origGrid
+
+        # TODO return scores along with winning char ID for later use
+        return min(scores, key=scores.get)
 
 
     def putBetter(self, row, col, k):
         self.stats['positionsVisited'] += 1
         startX, startY, endX, endY = self.getSliceBounds(row, col)
-        targetSlice = self.targetImg[startY:endY, startX:endX]
-        # Brighten the target to match maximum typable in first overtype
-        targetSlice = gammaCorrect(targetSlice, self.maxGamma)
-        # Get ID of best match
-        if k == 1:
-            bestMatch = self.getFirstBetter(targetSlice, row, col)
-        else:
-            # bestMatch = self.getBestOfRandomK(targetSlice, row, col, k)
-            bestMatch = self.getBestOfClosestK(targetSlice, row, col, k)
+        
+        bestMatch = self.getBestOfRandomK(row, col, k)
 
         if bestMatch:
             # print(self.comboGrid.get(row, col), bestMatch)
+            changed = True
             self.comboGrid.put(row, col, bestMatch)
             self.mockupImg[startY:endY, startX:endX] = self.compositeAdj(row, col)
-            return True
         else:
             # print("already good")
-            self.comboGrid.clean(row, col)
-            return False
+            changed = False
+            # self.comboGrid.clean(row, col)
 
+        self.ditherImg = self.applyDither(row, col)
+        return changed
+
+
+        # Only uses MSE
+    def getBestOfRandomK(self, row, col, k=5, binned=False):
+        
+        # Score against temporary ditherImg created for this comparison
+        def compare(row, col, ditherImg):
+            startX, startY, endX, endY = self.getSliceBounds(row, col)
+            targetSlice = ditherImg[startY:endY, startX:endX]
+            mockupSlice = self.compositeAdj(row, col)
+            # brighten targetSlice
+            err = abs(np.average(targetSlice/self.maxGamma - mockupSlice))
+            targetSlice = gammaCorrect(targetSlice, self.maxGamma)
+            mse = np.sqrt(compare_mse(targetSlice, mockupSlice))
+            # print('err:',err,'mse:',mse)
+            return err + mse
+
+        ditherImg = self.ditherImg
+        curScore = compare(row, col, ditherImg)
+        if binned:
+            chars = self.charSet.getSorted()
+            # Always include space
+            charIdx = [0]
+            for i in range(k):
+                startIdx = max(1, (len(chars)*i)//k)
+                endIdx = (len(chars)*(i+1))//k
+                charIdx.append(np.random.randint(startIdx, endIdx))
+            chars = list(np.array(chars)[charIdx])
+        else:
+            chars = self.charSet.getAll()
+            chars = np.random.choice(chars, k, replace=False)
+        scores = {}
+        origGrid = self.comboGrid.grid.copy()
+        for char in chars:
+            self.stats['comparisonsMade'] += 1
+            self.comboGrid.put(row, col, char.id)
+            ditherImg = self.applyDither(row, col)
+            # Score the composite
+            scores[char.id] = compare(row, col, ditherImg)
+
+        self.comboGrid.grid = origGrid
+
+        bestChoice = min(scores, key=scores.get)
+        better = scores[bestChoice] < curScore
+        return bestChoice if better else None
+
+
+    # Return updated copy of the dither image based on current selection
+    def applyDither(self, row, col, amount=0.5):
+        # print("Begin dither")
+        ditherImg = self.ditherImg.copy()
+
+        startX, startY, endX, endY = self.getSliceBounds(row, col)
+        ditherSlice = ditherImg[startY:endY, startX:endX]
+        mockupSlice = self.mockupImg[startY:endY, startX:endX]
+        ditherDone = np.zeros(ditherSlice.shape, dtype=np.bool)
+
+        # Li dither by pixel
+        residual = 0
+        h, w = ditherSlice.shape
+        K = 2.6 # Hyperparam
+        M = 5   # Mask size
+        c = M//2
+
+        # Calculate error between chosen combo and target subslice
+        # Per pixel
+        for row in range(len(ditherSlice)):
+            for col in range(len(ditherSlice[row])):
+                actual = mockupSlice[row, col]
+                target = ditherSlice[row, col]
+                error = target - actual
+                # print(error)
+                # print(ditherSlice.shape, mockupSlice.shape)
+                # Get adjacent pixels which aren't chosen already, checking bounds
+                adjIdx = []
+                for i in range(max(0,row-c), min(h, row+c+1)):
+                    for j in range(max(0,col-c), min(w, col+c+1)):
+                        # print(i,j)
+                        if (i, j) != (row, col) and not ditherDone[i, j]:
+                            adjIdx.append(
+                                (i, j, np.linalg.norm(np.array([i,j])-np.array([row,col])))
+                            )
+
+                weightIdx = []
+                for i, j, dist in adjIdx:
+                    adjVal = ditherSlice[i, j]
+                    # Darken slices which are already darker, and vice-versa
+                    # Affect closer slices more
+                    weight = (adjVal if error > 0 else 255 - adjVal) / (dist**K)
+                    weightIdx.append((i, j, weight, adjVal))
+                
+                totalWeight = np.sum([weight for _, _, weight, _ in weightIdx])
+                for i, j, weight, beforeVal in weightIdx:
+                    # Normalize weights since not all slices will be adjustable
+                    weight /= totalWeight
+                    # Overall we want to reach this level with the slice:
+                    desiredVal = beforeVal + error*weight + residual
+                    # Apply corrections per pixel
+                    correction = (desiredVal - beforeVal)
+                    ditherSlice[i, j] = min(255, max(0, ditherSlice[i, j] + correction))
+                    afterVal = ditherSlice[i, j]
+                    residual = desiredVal - afterVal
+                    # print(beforeVal, desiredVal - afterVal)
+
+        # print("end dither")
+        return ditherImg
+        
 
     # Uses combos to store already composited "full" (all 4 layers)
     # If combo not already generated, add it to comboSet.
+    # Returns mockupImg slice
     def compositeAdj(self, row, col):
         
         def getIndices(cDict):
@@ -136,144 +273,9 @@ class Generator:
         return img
 
 
-    # Try compositing different characters onto a copy of the mockupSlice
-    # Compare each to the targetSlice
-    # Return the id of the best matching character
-    def getBestAdj(self, targetSlice, row, col):
-        # TODO speed up search by taking advantage of sorted order
-        chars = self.charSet.getAll()
-        scores = {}
-        scores2 = {}
-        origGrid = self.comboGrid.grid.copy()
-        for char in chars:
-            self.stats['comparisonsMade'] += 1
-            self.comboGrid.put(row, col, char.id)
-            newMockup = self.compositeAdj(row, col)
-            # Score the composite
-            if self.compareMode == 'mse':
-                scores[char.id] = compare_mse(targetSlice, newMockup)
-            elif self.compareMode == 'ssim':
-                scores[char.id] = -1 * compare_ssim(targetSlice, newMockup)
-            elif self.compareMode == 'blend':
-                scores[char.id] = compare_mse(targetSlice, newMockup) 
-                scores2[char.id] = -1 * compare_ssim(targetSlice, newMockup) + 1
-                self.stats['comparisonsMade'] += 1
-            else:
-                print('generator: invalid compareMode')
-                exit()
-
-        self.comboGrid.grid = origGrid
-
-        if self.compareMode == 'blend':
-            fMSE=sum(scores.values())
-            fSSIM=sum(scores2.values())
-            for k in scores:
-                scores[k] = (scores[k]/fMSE) * (scores2[k]/fSSIM)
-
-        # TODO return scores along with winning char ID for later use
-        return min(scores, key=scores.get)
-
-
-    # Only uses MSE
-    def getBestOfRandomK(self, targetSlice, row, col, k=5, binned=False):
-        # Get score of existing slice
-        startX, startY, endX, endY = self.getSliceBounds(row, col)
-        mockupSlice = self.mockupImg[startY:endY, startX:endX]
-        curScore = compare_mse(targetSlice, mockupSlice)
-        if binned:
-            chars = self.charSet.getSorted()
-            # Always include space
-            charIdx = [0]
-            for i in range(k):
-                startIdx = max(1, (len(chars)*i)//k)
-                endIdx = (len(chars)*(i+1))//k
-                charIdx.append(np.random.randint(startIdx, endIdx))
-            chars = list(np.array(chars)[charIdx])
-        else:
-            chars = self.charSet.getAll()
-            chars = np.random.choice(chars, k, replace=False)
-        scores = {}
-        origGrid = self.comboGrid.grid.copy()
-        for char in chars:
-            self.stats['comparisonsMade'] += 1
-            self.comboGrid.put(row, col, char.id)
-            newMockup = self.compositeAdj(row, col)
-            # Score the composite
-            scores[char.id] = compare_mse(targetSlice, newMockup)
-
-        self.comboGrid.grid = origGrid
-
-        bestChoice = min(scores, key=scores.get)
-        better = scores[bestChoice] < curScore
-        return bestChoice if better else None
-
-    # Only uses MSE
-    def getBestOfClosestK(self, targetSlice, row, col, k=5):
-        # Get score of existing slice
-        startX, startY, endX, endY = self.getSliceBounds(row, col)
-        mockupSlice = self.mockupImg[startY:endY, startX:endX]
-        curScore = compare_mse(targetSlice, mockupSlice)
-        chars = self.charSet.getSorted()
-        startPos = int((1 - (np.average(targetSlice) / 255)) * len(chars)) - k
-        endPos = startPos + 2*k
-        if startPos < 0:
-            endPos -= startPos
-            startPos = 0
-        if endPos > len(chars):
-            startPos -= endPos - len(chars)
-            endPos = len(chars)
-        # print(startPos, endPos)
-        chars = chars[startPos:endPos]
-        scores = {}
-        origGrid = self.comboGrid.grid.copy()
-        for char in chars:
-            self.stats['comparisonsMade'] += 1
-            self.comboGrid.put(row, col, char.id)
-            newMockup = self.compositeAdj(row, col)
-            # Score the composite
-            scores[char.id] = compare_mse(targetSlice, newMockup)
-
-        self.comboGrid.grid = origGrid
-
-        bestChoice = min(scores, key=scores.get)
-        better = scores[bestChoice] < curScore
-        return bestChoice if better else None
-
-
-    # Only uses MSE
-    def getFirstBetter(self, targetSlice, row, col):
-        # Get score of existing slice
-        startX, startY, endX, endY = self.getSliceBounds(row, col)
-        mockupSlice = self.mockupImg[startY:endY, startX:endX]
-        curScore = compare_mse(targetSlice, mockupSlice)
-        chars = self.charSet.getAll()[:]
-        np.random.shuffle(chars)
-        scores = {}
-        origGrid = self.comboGrid.grid.copy()
-        for char in chars:
-            self.stats['comparisonsMade'] += 1
-            self.comboGrid.put(row, col, char.id)
-            newMockup = self.compositeAdj(row, col)
-            # Score the composite
-            if compare_mse(targetSlice, newMockup) < curScore:
-                self.comboGrid.grid = origGrid
-                return char.id
-        self.comboGrid.grid = origGrid
-        return None
-
-
     def generateLayers(self, compareModes=['m'], numAdjustPasses=0,
                         show=True, mockupFn='mp_untitled', gamma=1,
                         randomInit=False, randomOrder=False):
-        
-        modeDict = {
-            'm':'mse',
-            's':'ssim',
-            'b':'blend'
-        }
-        compareModes = [modeDict[c] for c in compareModes]
-
-        self.maxGamma = gamma
 
         def dirtyLinearPositions(randomize=False):
             positions = []
@@ -290,47 +292,39 @@ class Generator:
                     for col in range(startCol, endCol, 2):
                         if self.comboGrid.isDirty(row,col):
                             positions.append((row, col))
+                positions.append(None)
             if randomize:
                 np.random.shuffle(positions)
             return positions
 
         def setupFig():
-            fig = plt.figure(frameon=False)
-            fig.set_size_inches(5,5)
-            ax1 = plt.Axes(fig, [0., 0., 1., 1.])
-            ax1.set_axis_off()
-            fig.add_axes(ax1)
-            ax1.get_xaxis().set_visible(False)
-            ax1.get_yaxis().set_visible(False)
-            ax1.axis('off')
-            fig.subplots_adjust(bottom = 0)
-            fig.subplots_adjust(top = 1)
-            fig.subplots_adjust(right = 1)
-            fig.subplots_adjust(left = 0)
-            fig.gca().set_frame_on(False)
-            return fig, ax1
+            fig, ax = plt.subplots(1, 2, sharex='col', sharey='row')
+            return fig, ax
 
+        modeDict = {
+            'm':'mse',
+            's':'ssim',
+            'b':'blend'
+        }
+        compareModes = [modeDict[c] for c in compareModes]
+        self.maxGamma = gamma
         self.compareMode = compareModes.pop(0)
-        # For top left layer, start at 0,0. For bottom left 1,0. Etc.
-        # Using None to indicate when we are switching to another layer
         self.positions = dirtyLinearPositions(randomize=randomOrder)
 
         # Initialize randomly if desired
         numChars = len(self.charSet.getAll())
         if randomInit:
             while len(self.positions) > 0:
-                row, col = self.positions.pop(0)
+                pos = self.positions.pop(0)
+                if pos is None:
+                    continue
+                row, col = pos
                 startX, startY, endX, endY = self.getSliceBounds(row, col)
                 self.comboGrid.put(row, col, np.random.randint(1, numChars+1))
                 self.mockupImg[startY:endY, startX:endX] = self.compositeAdj(row, col)
 
-        fig, ax1 = setupFig()
+        fig, ax = setupFig()
         self.adjustPass = 0
-
-        def genFrames():
-            while len(self.positions) > 0 or len(dirtyLinearPositions()) > 0:
-                yield 1
-
         printEvery = 50
 
         def animate(frame):
@@ -345,12 +339,18 @@ class Generator:
                 # print(self.comboGrid)
                 self.positions += dirtyLinearPositions(randomize=randomOrder)
                 # print("dirty:", len(self.positions))
-            row, col = self.positions.pop(0)
-            if self.putBestAdj(row, col):
-            # if self.putBetter(row, col, 44): # best of k random
+            pos = self.positions.pop(0)
+            if pos is None:
+                self.ditherImg = self.targetImg.copy()
+                return
+            row, col = pos
+            # if self.putBestAdj(row, col):
+            if self.putBetter(row, col, 5): # best of k random
             # if self.putBetter(row, col, 1): # first random better
-                ax1.clear()
-                ax1.imshow(self.mockupImg, cmap='gray')
+                ax[0].clear()
+                ax[0].imshow(self.mockupImg, cmap='gray')
+            ax[1].clear()
+            ax[1].imshow(self.ditherImg, cmap='gray')
 
         # numFrames = (len(self.positions)-4)*(len(compareModes)+1+numAdjustPasses)
         Writer = animation.writers['ffmpeg']
